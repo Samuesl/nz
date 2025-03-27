770a53226f2fb7ef0d517bff1b82a84bbf73e4ce
Samuesl <ruess.samuel@gmail.com>
2025-03-27 17:20:40 +0100

    fehler im Etappenobjekt bereinigt

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const STOPS = [
         lng: 172.6775,
         zoom: 13,
         title: "Cape Reinga",
-        nummer: 1,
+        nr: 1,
         user: "vintiyannick",
     },
     {
@@ -25,7 +25,7 @@ const STOPS = [
         title: "Bay of Islands",
         user: "Ellinnaa",
         lat:-35.183333,
-        long:174.166667,
+        lng:174.166667,
         zoom:11,
     },
     {
@@ -121,7 +121,7 @@ const STOPS = [
         title: "Wanaka",
         user: "lizzie2911",
         lat: -44.7,
-        lng:169.15,
+        lng: 169.15,
         zoom: 13,
     },
     {
