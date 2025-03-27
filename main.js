9209bfda6b0a086151982566eb423f9d9711c48d
Samuesl <ruess.samuel@gmail.com>
2025-03-27 15:36:14 +0100

    Kommentare in main.js ergänzt

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+/*
+Skript fuer die Neuseelandreise
+*/
+//Karte inititlaisieren
 var map = L.map('map').setView([-36.83, 174.8], 13);
+
+//Hintergrundkarte realisieiren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 <!--Koordinate um 100el erweitert, damit der Marker in der Mitte sitzt-->
+
+//Marker zeinchen
 var marker = L.marker([-36.831, 174.81]).addTo(map);
+
+//Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup(); 
